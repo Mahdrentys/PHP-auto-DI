fb71915fd9515c88ed48a00512469d17c035156c
Mahdrentys <mahdrentys@gmail.com>
2018-09-09 18:25:54 +0200

    Write tests

--- tests/ContainerSpec.php
+++ tests/ContainerSpec.php
@@ -15,18 +15,20 @@ class A
 class B
 {
     public $uniqid;
+    public $a;
 
-    public function __construct()
+    public function __construct(A $a)
     {
         $this->uniqid = uniqid();
+        $this->a = $a;
     }
 }
 
 $container = Container::getContainer();
 $container->set('a', new A());
-$container->set('b', function()
+$container->set('b', function() use ($container)
 {
-    return new B();
+    return new B($container->get('a'));
 });
 
 describe('Container', function()
@@ -78,6 +80,23 @@ describe('Container', function()
         $b1 = $container->build('b');
         $b2 = $container->build('b');
         expect($b1->uniqid != $b2->uniqid)->toBe(true);
+    });
+
+    it('should resolve empty constructor', function()
+    {
+        $container = Container::getContainer();
+
+        $a = $container->get(A::class);
+        expect($a)->toBeAnInstanceOf(A::class);
+    });
+
+    it('should resolve constructor with a dependency', function()
+    {
+        $container = Container::getContainer();
+
+        $b = $container->get(B::class);
+        expect($b)->toBeAnInstanceOf(B::class);
+        expect($b->a)->toBeAnInstanceOf(A::class);
     });
 
 });
