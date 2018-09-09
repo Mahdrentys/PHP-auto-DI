0453923d5a9a656b42962648d16cf3476c2608e7
Mahdrentys <mahdrentys@gmail.com>
2018-09-09 15:53:28 +0200

    set method

--- src/Container.php
+++ src/Container.php
@@ -3,14 +3,24 @@
 namespace Mahdrentys\AutoDI;
 
 use Psr\Container\ContainerInterface;
+use Closure;
 
 class Container implements ContainerInterface
 {
     private static $container = null;
+    private $factories = [];
+    private $instances = [];
 
     public function set($key, $value):void
     {
-
+        if (is_object($value) AND $value instanceof Closure)
+        {
+            $this->factories[$key] = $value;
+        }
+        else
+        {
+            $this->instances[$key] = $value;
+        }
     }
 
     public function get($key)
