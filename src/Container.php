a9ab7328554cab16f241cf2cba86fb76118a0094
Mahdrentys <mahdrentys@gmail.com>
2018-09-09 15:25:02 +0200

    Created Container class

---
+++ src/Container.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mahdrentys\AutoDI;
+
+use Psr\Container\ContainerInterface;
+
+class Container implements ContainerInterface
+{
+    public function set($key, $value):void
+    {
+
+    }
+
+    public function get($key)
+    {
+
+    }
+
+    public function has($key):bool
+    {
+
+    }
+
+    public static function getContainer():ContainerInterface
+    {
+
+    }
+}
